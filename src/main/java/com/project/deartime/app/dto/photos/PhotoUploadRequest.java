dea6cc96819997d429b8b43637ba4aa2a450ac34
dmseong <149465698+dmseong@users.noreply.github.com>
2025-12-28 20:13:27 +0900

    fix: input 중첩 요소 제거

--- src/main/java/com/project/deartime/app/dto/photos/PhotoUploadRequest.java
+++ src/main/java/com/project/deartime/app/dto/photos/PhotoUploadRequest.java
@@ -1,9 +1,5 @@
 package com.project.deartime.app.dto.photos;
 
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
-
 public record PhotoUploadRequest (
         String caption,
         Long albumId
