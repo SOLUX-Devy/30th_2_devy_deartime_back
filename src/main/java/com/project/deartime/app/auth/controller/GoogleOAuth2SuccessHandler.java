907b008bb169eb3b6e44c16c4523b72aebcb9521
yoonjae-kwon <yoonjae.kwon03@gmail.com>
2026-01-23 00:55:01 +0900

    Revert "fix: 구글 리다이렉션 로직 오류 수정&도메인 추가"
    This reverts commit 66c155e3467b28077777a18930c5315c13f9169b.

--- src/main/java/com/project/deartime/app/auth/controller/GoogleOAuth2SuccessHandler.java
+++ src/main/java/com/project/deartime/app/auth/controller/GoogleOAuth2SuccessHandler.java
@@ -34,25 +34,14 @@ public class GoogleOAuth2SuccessHandler implements AuthenticationSuccessHandler 
         // 요청 헤더에서 Origin 또는 Referer를 확인하여 동적으로 리다이렉트 URL 결정
         String origin = request.getHeader("Origin");
         String referer = request.getHeader("Referer");
-        String host = request.getServerName(); // 요청 받은 서버의 도메인 확인
 
         String frontendUrl;
         if (origin != null && origin.contains("localhost")) {
             frontendUrl = "http://localhost:5173";
         } else if (referer != null && referer.contains("localhost")) {
             frontendUrl = "http://localhost:5173";
-        } else if (host.equals("localhost") || host.equals("127.0.0.1")) {
-            // Referer가 구글이어도 현재 서버가 로컬이면 로컬 프론트로 이동
-            frontendUrl = "http://localhost:5173";
-
-        } else if (referer != null && referer.contains("vercel.app")) {
-            // "vercel.app"에서 왔으면 다시 거기로 보내줌(도메인 작업 이전)
-            frontendUrl = "https://30th-2-devy-deartime-front.vercel.app";
-
-        // 3. 정식 배포 환경 (새 도메인)
         } else {
-            // 그 외의 모든 경우(나중에 도메인 연결되면 여기로 옴)는 정식 도메인으로
-            frontendUrl = "https://deartime.kr";
+            frontendUrl = "https://30th-2-devy-deartime-front.vercel.app";
         }
 
         String callbackUrl = frontendUrl + "/oauth/callback";
